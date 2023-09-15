0f43fef5a1f6d4bc7fa0c6de8bf90cfc11f4cfb4
김종혁 <notify9637@naver.com>
2023-09-15 17:49:29 +0900

    모델과 변수명 일치

--- mysite02/src/main/java/com/poscodx/mysite/vo/BoardVo.java
+++ mysite02/src/main/java/com/poscodx/mysite/vo/BoardVo.java
@@ -1,101 +1,83 @@
 package com.poscodx.mysite.vo;
 
 public class BoardVo {
-	private int boardNo;
+	private int no;
 	private String title;
 	private String contents;
 	private int hit;
-	private String date;
+	private String reg_date;
 	private int g_no;
 	private int o_no;
 	private int depth;
 	private long user_no;
 	private String userName;
-
-	public long getUser_no() {
-		return user_no;
+	public int getNo() {
+		return no;
 	}
-
-	public void setUser_no(long user_no) {
-		this.user_no = user_no;
+	public void setNo(int no) {
+		this.no = no;
 	}
-
-	public int getBoardNo() {
-		return boardNo;
-	}
-
-	public void setBoardNo(int boardNo) {
-		this.boardNo = boardNo;
-	}
-
 	public String getTitle() {
 		return title;
 	}
-
 	public void setTitle(String title) {
 		this.title = title;
 	}
-
 	public String getContents() {
 		return contents;
 	}
-
 	public void setContents(String contents) {
 		this.contents = contents;
 	}
-
 	public int getHit() {
 		return hit;
 	}
-
 	public void setHit(int hit) {
 		this.hit = hit;
 	}
-
-	public String getDate() {
-		return date;
+	public String getReg_date() {
+		return reg_date;
 	}
-
-	public void setDate(String date) {
-		this.date = date;
+	public void setReg_date(String reg_date) {
+		this.reg_date = reg_date;
 	}
-
 	public int getG_no() {
 		return g_no;
 	}
-
 	public void setG_no(int g_no) {
 		this.g_no = g_no;
 	}
-
 	public int getO_no() {
 		return o_no;
 	}
-
 	public void setO_no(int o_no) {
 		this.o_no = o_no;
 	}
-
 	public int getDepth() {
 		return depth;
 	}
-
 	public void setDepth(int depth) {
 		this.depth = depth;
 	}
-
+	public long getUser_no() {
+		return user_no;
+	}
+	public void setUser_no(long user_no) {
+		this.user_no = user_no;
+	}
 	public String getUserName() {
 		return userName;
 	}
-
 	public void setUserName(String userName) {
 		this.userName = userName;
 	}
-
 	@Override
 	public String toString() {
-		return "BoardVo [boardNo=" + boardNo + ", title=" + title + ", contents=" + contents + ", hit=" + hit
-				+ ", date=" + date + ", g_no=" + g_no + ", o_no=" + o_no + ", depth=" + depth + "]";
+		return "BoardVo [no=" + no + ", title=" + title + ", contents=" + contents + ", hit=" + hit + ", reg_date="
+				+ reg_date + ", g_no=" + g_no + ", o_no=" + o_no + ", depth=" + depth + ", user_no=" + user_no
+				+ ", userName=" + userName + "]";
 	}
+
+	
 
 }
