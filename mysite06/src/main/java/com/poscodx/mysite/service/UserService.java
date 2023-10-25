d788851e95c9f91dd5e6ba4e1531c0526ba7d917
김종혁 <notify9637@naver.com>
2023-10-25 13:55:22 +0900

    guestbook ajax init

--- mysite06/src/main/java/com/poscodx/mysite/service/UserService.java
+++ mysite06/src/main/java/com/poscodx/mysite/service/UserService.java
@@ -16,11 +16,11 @@ public class UserService {
 
 	public void join(UserVo vo) {
 		System.out.println(vo);
-		
+
 		userRepository.insert(vo);
-		
+
 		System.out.println(vo);
-		
+
 		// mailSender.send(vo.getEmail(), "", "");
 	}
 
@@ -30,6 +30,10 @@ public class UserService {
 
 	public UserVo getUser(Long no) {
 		return userRepository.findByNo(no);
+	}
+
+	public UserVo getUser(String email) {
+		return userRepository.findByEmail(email);
 	}
 
 	public void update(UserVo userVo) {
